79c5707ec1fb58081fefa3756fb3d2266ce6e597
William del Rosario <williamdelrosario@yahoo.com>
2018-04-10 18:13:25 +0200

    Restoring redis config

--- services/cache.js
+++ services/cache.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
