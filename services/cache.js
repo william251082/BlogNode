00590fbf73ab7cdf2d09f07aae091d2c515cdaf6
William del Rosario <williamdelrosario@yahoo.com>
2018-04-10 16:24:48 +0200

    Unique Keys

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,9 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
     console.log('IM ABOUT TO RUN A QUERY');
+
+    console.log(this.getQuery());
+    console.log(this.mongooseCollection.name);
+
     return exec.apply(this, arguments);
 }
