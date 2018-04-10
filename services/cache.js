589f03c0ca560d4debffca6a6eb950f6eb3f7db5
William del Rosario <williamdelrosario@yahoo.com>
2018-04-10 20:06:04 +0200

    Clearing nested hashes

--- services/cache.js
+++ services/cache.js
@@ -41,5 +41,10 @@ mongoose.Query.prototype.exec = async function () {
     client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
     return result;
+};
 
-}
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
+    }
+};
