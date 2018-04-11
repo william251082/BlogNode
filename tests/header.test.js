b643bf43225d4766e503a8c35e74ad54d7a6f2b5
William del Rosario <williamdelrosario@yahoo.com>
2018-04-11 08:22:51 +0200

    DRY tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,15 @@
 const puppeteer = require('puppeteer');
 
+let browser, page;
+
+beforeEach( async () => {
+    browser = await puppeteer.launch({
+      headless: false
+    });
+    page = await browser.newPage();
+    await  page.goto('localhost:3000');
+});
+
 test('Adds two numbers', () => {
    const sum = 1 + 2;
 
@@ -8,12 +18,6 @@ test('Adds two numbers', () => {
 });
 
 test('We can launch a browser', async () => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
-    await  page.goto('localhost:3000');
-
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
     expect(text).toEqual('Blogster');
