55c68d9f2fccfbbdcf6beb4c849d04d66bfdb3cb
William del Rosario <williamdelrosario@yahoo.com>
2018-04-12 12:55:57 +0200

    The Session Factory

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,7 +11,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-    // await browser.close();
+    await browser.close();
 });
 
 test('Adds two numbers', () => {
@@ -35,23 +35,8 @@ test('Clicking login starts oauth flow', async () => {
     expect(url).toMatch(/accounts\.google\.com/);
 });
 
-test.only('When signed in, shows logout button', async () => {
+test('When signed in, shows logout button', async () => {
     const id = '5acc9d2efd7888634168aed5';
-
-    const Buffer = require('safe-buffer').Buffer;
-    const sessionObject = {
-        passport: {
-            user: id
-        }
-    };
-    const sessionString = Buffer.from(
-        JSON.stringify(sessionObject)
-    ).toString('base64');
-
-    const Keygrip = require('keygrip');
-    const keys = require('../config/keys');
-    const keygrip = new Keygrip([keys.cookieKey]);
-    const sig = keygrip.sign('session=' + sessionString);
 
     await page.setCookie({ name: 'session', value: sessionString });
     await page.setCookie({ name: 'session.sig', value: sig});
