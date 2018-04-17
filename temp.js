2beabba1a460d30c30ad0909a2ba02ec2161abb7
William del Rosario <williamdelrosario@yahoo.com>
2018-04-17 20:48:35 +0200

    A final get test

--- temp.js
+++ temp.js
@@ -1,12 +1,22 @@
 // Temporary file to create a new blog post
 
+// () => {
+//     fetch('/api/blogs', {
+//         method: 'POST',
+//         credentials: 'same-origin',
+//         headers: {
+//             'Content-type': 'application/json'
+//         },
+//         body: JSON.stringify({title: 'My Title', content: 'My Content'})
+//     });
+// }
+
 () => {
     fetch('/api/blogs', {
-        method: 'POST',
+        method: 'GET',
         credentials: 'same-origin',
         headers: {
             'Content-type': 'application/json'
-        },
-        body: JSON.stringify({title: 'My Title', content: 'My Content'})
+        }
     });
 }
